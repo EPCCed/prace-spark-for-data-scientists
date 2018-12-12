9e1a9c488fd98bca1f426d5038b25a6225e32ce9
Rosa Filgueira <rosa.filgueira@ed.ac.uk>
2018-12-12 14:00:00 -0500

    Update start_Jupyter_local.sh
    ssh rosaf2@login.cirrus.ac.uk -L8888:r1i4n35:8888

--- notebooks_collection/start_Jupyter_local.sh
+++ notebooks_collection/start_Jupyter_local.sh
@@ -1,2 +1,1 @@
-#PYSPARK_PYTHON=/usr/local/anaconda/bin/python3.6 PYSPARK_DRIVER_PYTHON=jupyter PYSPARK_DRIVER_PYTHON_OPTS="notebook --ip=0.0.0.0 --allow-root" /usr/local/spark/bin/pyspark
-PYSPARK_PYTHON=/lustre/sw/anaconda/anaconda3-5.1.0/bin/python3 PYSPARK_DRIVER_PYTHON=jupyter PYSPARK_DRIVER_PYTHON_OPTS="notebook" $HOME/spark-2.4.0-bin-hadoop2.7/bin/pyspark
+PYSPARK_PYTHON=/lustre/sw/anaconda/anaconda3-5.1.0/bin/python3 PYSPARK_DRIVER_PYTHON=jupyter PYSPARK_DRIVER_PYTHON_OPTS="notebook --ip=0.0.0.0 --allow-root" $HOME/spark-2.4.0-bin-hadoop2.7/bin/pyspark
