82d6d2caa0019c91d25ce66b86c969d2e59ae136
rosafilgueira <rosa.filgueira@epcc.ed.ac.uk>
2019-01-09 23:09:11 +0000

    wordcount.py

--- Spark_Applications/wordcount.py
+++ Spark_Applications/wordcount.py
@@ -4,8 +4,8 @@ from pyspark import SparkContext, SparkConf
 if __name__ == "__main__":
     conf = SparkConf().setAppName("Spark Count")
     sc = SparkContext(conf=conf)
-    logFile = "../spark-2.4.0-bin-hadoop2.7/README.md"
-    textFile = sc.textFile(logFile)
+    inputFile = sys.argv[1]
+    textFile = sc.textFile(inputFile)
     wordCounts = textFile.flatMap(lambda line: line.split()).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a+b)
     output=wordCounts.collect()
     for (word, count) in output:
