92b56025761251e399e0cdf386d0f7371ea55a7b
akrause2014 <akrause2014@users.noreply.github.com>
2019-05-15 10:32:37 +0200

    Update start_Jupyter_local.sh

--- lab_exercises/start_Jupyter_local.sh
+++ lab_exercises/start_Jupyter_local.sh
@@ -1,1 +1,1 @@
-PYSPARK_PYTHON=/lustre/sw/anaconda/anaconda3-5.1.0/bin/python3 PYSPARK_DRIVER_PYTHON=jupyter PYSPARK_DRIVER_PYTHON_OPTS="notebook --ip=0.0.0.0 --allow-root" $HOME/spark-2.4.0-bin-hadoop2.7/bin/pyspark
+PYSPARK_PYTHON=/lustre/sw/anaconda/anaconda3-5.1.0/bin/python3 PYSPARK_DRIVER_PYTHON=jupyter PYSPARK_DRIVER_PYTHON_OPTS="notebook --ip=0.0.0.0 --allow-root" $HOME/spark-2.4.0-bin-hadoop2.7/bin/pyspark spark://<MASTER_NODE>:7077
