3130522dc07251e6fe06ee93dd104ac3ff408a63
rosafilgueira <rosaf3@indy2-login0.ib0.icexa.epcc.ed.ac.uk>
2019-10-28 14:58:30 +0000

    spark_submit_SparkPi.sh adding argument master node

--- Spark_Applications/spark_submit_SparkPi.sh
+++ Spark_Applications/spark_submit_SparkPi.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-set -x 
 
 $HOME/spark-2.4.0-bin-hadoop2.7/bin/spark-submit --verbose --class org.apache.spark.examples.SparkPi  --master spark://$1:7077 --deploy-mode cluster --supervise $HOME/spark-2.4.0-bin-hadoop2.7/examples/jars/spark-examples_2.11-2.4.0.jar  10
+
